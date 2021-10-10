c5ea466e271659e98094afda54180c5d7e5db31b
Irfan Yunus Soydan <irfanysoydan@gmail.com>
2021-10-10 15:15:09 +0300

    Basic Javascript Touches

---
+++ js/script.js
@@ -0,0 +1,40 @@
+/* Scroll Reveal Animations */
+
+ScrollReveal().reveal('.ani-left', {
+    origin: 'left',
+    distance: '10rem',
+    duration: 750,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-right', {
+    origin: 'right',
+    distance: '10rem',
+    duration: 750,
+    easing: 'ease-in'
+});
+
+ScrollReveal().reveal('.ani-fade-up', {
+    opacity: 0,
+    easing: 'ease-in'
+});
+
+
+ScrollReveal().reveal('.ani-bottom', {
+    origin: 'bottom',
+    distance: '10rem',
+    duration: 750,
+    easing: 'ease-out'
+});
+
+ScrollReveal().reveal('.ani-delay-0', {
+    delay: 500
+});
+
+ScrollReveal().reveal('.ani-delay-1', {
+    delay: 1000
+});
+
+ScrollReveal().reveal('.ani-delay-2', {
+    delay: 1500
+});
