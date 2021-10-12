d758719265951e88916d20516911a29ecdef7cda
Irfan Yunus Soydan <irfanysoydan@gmail.com>
2021-10-12 18:29:53 +0300

    Media Queries and Scroll Reveal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+/* Responsive Navigation Menu */
+
+const navMenu = () => {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navList = document.querySelector('.nav-list');
+
+    navbarToggler.addEventListener('click', () => {
+        navList.classList.toggle('nav-active');
+        navbarToggler.classList.toggle('toggle');
+    })
+};
+
+navMenu();
+
+
 /* Scroll Reveal Animations */
 
 ScrollReveal().reveal('.ani-left', {
